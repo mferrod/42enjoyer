dc9780c96ab313b4c32440926ea58724822e8849
Mariano Fernandez Rodero <marianof@c2r9s6.42malaga.com>
2024-08-01 17:15:40 +0200

    01-08-2024

--- so_long/src/errors.c
+++ so_long/src/errors.c
@@ -6,7 +6,7 @@
 /*   By: marianof <mariano@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 17:10:21 by marianof          #+#    #+#             */
-/*   Updated: 2024/08/01 12:30:53 by marianof         ###   ########.fr       */
+/*   Updated: 2024/08/01 16:41:55 by marianof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,6 @@ void	error_and_free(void *c)
 
 void	error_and_free_matrix(char **c)
 {
-	int	i;
-
-	i = 0;
 	write(2, "ERROR.\n", 7);
 	free_flood_map(c);
 	exit(1);
