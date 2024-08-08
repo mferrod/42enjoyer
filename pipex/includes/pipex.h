441043f27bb7a7a918145b17e445106a891527ea
Mariano Fernandez Rodero <marianof@c3r6s2.42malaga.com>
2024-08-08 22:06:15 +0200

    08-08-2024

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,17 +6,32 @@
 /*   By: marianof <mariano@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 20:55:45 by marianof          #+#    #+#             */
-/*   Updated: 2024/08/08 20:57:33 by marianof         ###   ########.fr       */
+/*   Updated: 2024/08/08 21:20:36 by marianof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include <errno.h>
+# include <fcntl.h>
+# include <sys/wait.h>
 # include <unistd.h>
+# include <sys/types.h>
 
-//MY LIBRARIES
-# include "../libft/libft.h"
+// MY LIBRARIES
 # include "../ft_printf/ft_printf.h"
+# include "../libft/libft.h"
+
+typedef struct s_data
+{
+	int		fd_in;
+	int		fd_out;
+	char	*cmd;
+	char	**args;
+	int		pipe[2];
+	pid_t	child;
+	pid_t	child_s;
+}			t_data;
 
 #endif
--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,13 +6,16 @@
 /*   By: marianof <mariano@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 20:57:56 by marianof          #+#    #+#             */
-/*   Updated: 2024/08/08 21:00:46 by marianof         ###   ########.fr       */
+/*   Updated: 2024/08/08 21:22:28 by marianof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int	main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **envp)
 {
+	t_data	data;
+
+	ft_memset(&data, 0, sizeof(t_data));
 	return (0);
 }
