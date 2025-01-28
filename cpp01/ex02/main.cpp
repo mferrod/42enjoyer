daee72f26f8fb480597afb0de75a3f22a0f0583d
Mariano Fernandez Rodero <marianof@c3r3s4.42malaga.com>
2025-01-28 14:11:25 +0100

    cpp01/ex02 reuploaded

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -4,7 +4,15 @@
 int main(void)
 {
 	std::string myStr = "HI THIS IS BRAIN";
-	std::string stringPtr;
-	std::string stringRef;
+	std::string *stringPtr = &myStr;
+	std::string &stringRef = myStr;
+
+	std::cout << &myStr << std::endl;
+	std::cout << stringPtr << std::endl;
+	std::cout << &stringRef << std::endl;
+
+	std::cout << myStr << std::endl;
+	std::cout << *stringPtr << std::endl;
+	std::cout << stringRef << std::endl;
 	return 0;
 }
