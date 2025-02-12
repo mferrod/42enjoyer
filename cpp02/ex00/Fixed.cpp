dd34f655ba419d496227d04c91026c8847eb80b2
Mariano Fernandez Rodero <marianof@c2r9s5.42malaga.com>
2025-02-12 15:00:33 +0100

    New text commented

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -2,24 +2,24 @@
 
 Fixed :: Fixed()
 {
-	std::cout << "Default constructor called." << std::endl;
+	//std::cout << "Default constructor called." << std::endl;
 	this->fixedPoint = 0;
 }
 
 Fixed :: Fixed(const Fixed &fix)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	//std::cout << "Copy constructor called" << std::endl;
 	*this = fix;
 }
 
 Fixed :: ~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
+	//std::cout << "Destructor called" << std::endl;
 }
 
 Fixed& Fixed :: operator=(const Fixed &fix)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
+	//std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &fix)
 		this->setRawBits(fix.getRawBits());
 	return *this;
@@ -27,7 +27,7 @@ Fixed& Fixed :: operator=(const Fixed &fix)
 
 int Fixed :: getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	//std::cout << "getRawBits member function called" << std::endl;
 	return this->fixedPoint;
 }
 
