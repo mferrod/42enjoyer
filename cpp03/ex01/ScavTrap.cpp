723ed6ea1cd109a07fab855f5334a9936c4b28ac
Mariano Fernández Rodero <marianofdezrodero@gmail.com>
2025-02-13 17:29:58 +0100

    cpp03/ex01

---
+++ cpp03/ex01/ScavTrap.cpp
@@ -0,0 +1,13 @@
+#include "ScavTrap.hpp"
+
+ScavTrap :: ScavTrap() {
+	std::cout << "SCAVTRAP: CONSTRUCTOR CALLED" << std::endl;
+}
+
+ScavTrap :: ~ScavTrap() {
+	std::cout << "SCAVTRAP: DECONSTRUCTOR CALLED" << std::endl;
+}
+
+void ScavTrap :: guardGate() {
+	
+}
---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,15 @@
+#include "ClapTrap.hpp"
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+class ScavTrap : ClapTrap {
+private:
+	
+public:
+	ScavTrap();
+	~ScavTrap();
+	void guardGate();
+};
+
+#endif
